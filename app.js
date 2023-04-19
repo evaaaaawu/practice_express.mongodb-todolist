b11d2df120d345874988c30d3e68641a0d082da8
Eva <eva@EvadeMacBook-Pro.local>
2023-04-19 14:40:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-// 載入 express 並建構應用程式伺服器
-const express = require('express')
+const express = require('express') // 載入 express 並建構應用程式伺服器
+const mongoose = require('mongoose') // 載入 mongoose
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+} //僅在非正式環境時, 使用 dotenv
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {

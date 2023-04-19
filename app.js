efde590c2ae0ae96732bb6e669cbaebbf0efc338
Eva <eva@EvadeMacBook-Pro.local>
2023-04-19 18:11:02 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express') // 載入 express 並建構應用程式伺服器
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser') // 引用 body-parser
+
 const Todo = require('./models/todo') // 載入 Todo model
 
 if (process.env.NODE_ENV !== 'production') {
@@ -24,12 +26,25 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000

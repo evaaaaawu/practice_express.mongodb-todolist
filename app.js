44e3ad115350b22ebef847c300c2b8c90412a372
Eva <eva@EvadeMacBook-Pro.local>
2023-04-19 16:30:05 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express') // 載入 express 並建構應用程式伺服器
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
+const Todo = require('./models/todo') // 載入 Todo model
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -19,9 +21,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000

b1917bb148a462120f9c9a335c6f500310142e13
Eva <eva@EvadeMacBook-Pro.local>
2023-04-25 19:16:33 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 載入 method-override
+const flash = require('connect-flash')   // 引用套件
 
 const routes = require('./routes') // 引用路由器
 
@@ -27,10 +28,13 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+app.use(flash())  // 掛載套件
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes) // 將 request 導入路由器

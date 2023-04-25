8e65b1c375a1a9827c86ce2abeaf3465c2ef0b21
Eva <eva@EvadeMacBook-Pro.local>
2023-04-25 11:55:31 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes) // 將 request 導入路由器
 
 // 設定 port 3000
